6ee0af6d34489c161dd1e167697d76106d44d680
Yevhen <yevhen_danylchenko@ukr.net>
2026-03-14 23:10:55 +0200

    Fix request my bot main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from My_Bot import *
 
+import uvicorn
 
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",          # main - назва файлу, app - об’єкт FastAPI
+        host="0.0.0.0",      # слухати всі інтерфейси
+        port=8000,           # порт, можна змінити
+        reload=True          # автоматичний перезапуск при зміні коду
+    )
