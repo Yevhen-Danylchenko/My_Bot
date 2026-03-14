f70511393909b1f9b2eea6ddc1f4c15700531769
Yevhen <yevhen_danylchenko@ukr.net>
2026-03-14 23:10:21 +0200

    Fix request my bot

--- My_Bot.py
+++ My_Bot.py
@@ -3,6 +3,9 @@ import requests
 
 from fastapi import  FastAPI, Request
 from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
